f5702f33e690ebe31d2ceed5816648b70546216c
Dmitry Bravikov <dmitry@bravikov.pro>
2016-11-02 22:48:26 +0500

    Added Russian localization.

---
+++ Translations/discover_ru.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../MainWindow.cpp" line="44"/>
+        <source>Network interface:</source>
+        <translation>Сетевой интерфейс:</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="77"/>
+        <source>Phasors and waveforms</source>
+        <translation>Векторные диаграммы и осциллограммы</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="78"/>
+        <source>Frequency analysis</source>
+        <translation>Частотный анализ</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="79"/>
+        <source>Symmetrical components</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="80"/>
+        <source>Power analysis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="81"/>
+        <source>Power quality</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="88"/>
+        <source>discover</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StreamTableModel</name>
+    <message>
+        <location filename="../StreamTableModel.cpp" line="96"/>
+        <source>Stream is being transmitted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../StreamTableModel.cpp" line="96"/>
+        <source>Stream has stopped being transmitted</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
